c85eda36e121c6d84258f2a460af7d7fcc1d3eeb
carsy <jlageb@gmail.com>
2016-01-10 16:25:11 +0000

    Filter should deal with functions returning promises

--- test/spec/filter.js
+++ test/spec/filter.js
@@ -31,3 +31,22 @@ test('filter(arr, fn)', (t) => {
         t.same(arr, input.slice(2, input.length));
     });
 });
+
+test('filter(arr, fn): filter function returns promise', (t) => {
+    const input = [1, 2, 3, 4];
+
+    return filter([Promise.resolve(1), 2, Promise.resolve(3), 4],
+        (n, i, array) => {
+            t.is(n, input[i]);
+            t.same(array, input);
+
+            return Promise.resolve()
+            .then(() => {
+                return n > 2;
+            });
+        }
+    )
+    .then((arr) => {
+        t.same(arr, input.slice(2, input.length));
+    });
+});
