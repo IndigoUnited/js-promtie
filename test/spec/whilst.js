5b2a0d585d5eebff3c2c2428d7a2a009064a3e6e
carsy <jlageb@gmail.com>
2016-02-06 19:19:24 +0000

    Added more whilst tests

--- test/spec/whilst.js
+++ test/spec/whilst.js
@@ -17,6 +17,26 @@ test('whilst(conditionFn, fn)', (t) => {
     .then(() => t.ok(lastCount > 3));
 });
 
+test('whilst(conditionFn, fn, options)', (t) => {
+    const start = Date.now();
+    let count = 0;
+    let lastCount = count;
+
+    setInterval(() => {
+        count += 1;
+    }, 20);
+
+    return whilst(() => {
+        lastCount = count;
+
+        return count <= 3;
+    }, () => t.ok(count <= 3), { delay: 100 })
+    .then(() => {
+        t.ok(lastCount > 3);
+        t.ok((Date.now() - start) > 100);
+    });
+});
+
 test('whilst(conditionFn, fn): conditionFn returns promise', (t) => {
     let count = 0;
     let lastCount = count;
@@ -78,6 +98,13 @@ test('whilst(conditionFn, fn): conditionFn throws', (t) => {
         throw new Error('condition not met');
     }, () => {
         t.fail('should not have run this');
+    })
+    .then(() => t.fail('expected to fail'), err => t.is(err.message, 'condition not met'));
+});
+
+test('whilst(conditionFn, fn): fn throws', (t) => {
+    return whilst(() => true, () => {
+        throw new Error('condition not met');
     })
     .then(() => t.fail('expected to fail'), err => t.is(err.message, 'condition not met'));
 });
