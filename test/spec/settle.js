d779bafd1360b58ad7da8f6c77baf9deccaeb003
carsy <jlageb@gmail.com>
2016-02-22 23:08:34 +0000

    Fix settle

--- test/spec/settle.js
+++ test/spec/settle.js
@@ -1,19 +1,17 @@
 import { settle } from '../../';
 import test from 'ava';
 
-test('settle(array)', t => {
-    return settle([Promise.resolve(1), Promise.reject(2)])
+test('settle(arr)', t => {
+    return settle([Promise.resolve(1), Promise.reject(new Error('second promise'))])
     .then(settledPromises => {
-        t.same(settledPromises, [{
-            fulfilled: true,
-            rejected: false,
-            value: 1,
-            reason: null,
-        }, {
-            fulfilled: false,
-            rejected: true,
-            value: null,
-            reason: 2,
-        }]);
+        t.ok(settledPromises[0].fulfilled);
+        t.notOk(settledPromises[0].rejected);
+        t.is(settledPromises[0].value, 1);
+        t.is(settledPromises[0].reason, null);
+
+        t.notOk(settledPromises[1].fulfilled);
+        t.ok(settledPromises[1].rejected);
+        t.is(settledPromises[1].value, null);
+        t.is(settledPromises[1].reason.message, 'second promise');
     });
 });
